c9acd63a744b54fe78f98895aee613cd47c3c0db
Carlos Ortiz <goodwine@google.com>
2017-03-24 17:44:55 -0700

    hg prev is not a vanilla command, use updated instead.

--- test/tagless_repo.ps1
+++ test/tagless_repo.ps1
@@ -4,4 +4,9 @@ set-location $args[0]
 new-item -type File "clean_file"
 hg add "clean_file"
 hg commit -m "Commit 1" -u "Tester <test@test.com>"
-hg prev
+
+new-item -type File "clean_file2"
+hg add "clean_file2"
+hg commit -m "Commit 2" -u "Tester <test@test.com>"
+
+hg checkout -r -2
--- test/tagless_repo.sh
+++ test/tagless_repo.sh
@@ -7,4 +7,9 @@ cd "$1"
 touch "clean_file"
 hg add "clean_file"
 hg commit -m "Commit 1" -u "Tester <test@test.com>"
-hg prev
+
+touch "clean_file2"
+hg add "clean_file2"
+hg commit -m "Commit 2" -u "Tester <test@test.com>"
+
+hg update -r -2
